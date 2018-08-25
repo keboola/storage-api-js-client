4691f110c0a2c7011ebf3e1b5915b7fe3d173619
Jakub Matejka <jakub@keboola.com>
2018-08-25 13:59:52 +0200

    feat: switch Storage errors to use http-errors

--- test/Buckets.js
+++ test/Buckets.js
@@ -8,36 +8,33 @@ describe('Storage.Buckets', () => {
 
   const bucketName1 = `bucket1-${_.random(1000, 2000)}`;
   const bucketId1 = `in.c-${bucketName1}`;
-  it('create', () =>
-    expect(() => storage.request('get', `buckets/${bucketId1}`), 'to be rejected')
-      .then(() => expect(() => storage.Buckets.create('in', bucketName1, { description: bucketName1 }), 'to be fulfilled'))
-      .then(() => storage.request('get', `buckets/${bucketId1}`))
-      .then((res) => {
-        expect(res, 'to have key', 'description');
-        expect(res.description, 'to be', bucketName1);
-      })
-      .then(() => storage.request('delete', `buckets/${bucketId1}`)));
+  it('create', () => expect(() => storage.request('get', `buckets/${bucketId1}`), 'to be rejected')
+    .then(() => expect(() => storage.Buckets.create('in', bucketName1, { description: bucketName1 }), 'to be fulfilled'))
+    .then(() => storage.request('get', `buckets/${bucketId1}`))
+    .then((res) => {
+      expect(res, 'to have key', 'description');
+      expect(res.description, 'to be', bucketName1);
+    })
+    .then(() => storage.request('delete', `buckets/${bucketId1}`)));
 
   const bucketName2 = `bucket2-${_.random(1000, 2000)}`;
   const bucketId2 = `in.c-${bucketName2}`;
-  it('get', () =>
-    expect(() => storage.Buckets.get(bucketId2), 'to be rejected')
-      .then(() => storage.request('post', 'buckets', { stage: 'in', name: bucketName2 }))
-      .then(() => storage.Buckets.get(bucketId2))
-      .then((res) => {
-        expect(res, 'to have key', 'id');
-        expect(res.id, 'to be', bucketId2);
-        expect(res, 'to have key', 'name');
-        expect(res.name, 'to be', `c-${bucketName2}`);
-      })
-      .then(() => storage.request('delete', `buckets/${bucketId2}`))
-      .then(() => expect(() => storage.Buckets.get(bucketId2), 'to be rejected')));
+  it('get', () => expect(() => storage.Buckets.get(bucketId2), 'to be rejected')
+    .then(() => storage.request('post', 'buckets', { stage: 'in', name: bucketName2 }))
+    .then(() => storage.Buckets.get(bucketId2))
+    .then((res) => {
+      expect(res, 'to have key', 'id');
+      expect(res.id, 'to be', bucketId2);
+      expect(res, 'to have key', 'name');
+      expect(res.name, 'to be', `c-${bucketName2}`);
+    })
+    .then(() => storage.request('delete', `buckets/${bucketId2}`))
+    .then(() => expect(() => storage.Buckets.get(bucketId2), 'to be rejected')));
 
   const bucketName3 = `bucket3-${_.random(1000, 2000)}`;
   const bucketId3 = `in.c-${bucketName3}`;
-  it('delete', () =>
-    expect(() => storage.Buckets.delete(bucketId3), 'to be rejected')
-      .then(() => storage.request('post', 'buckets', { stage: 'in', name: bucketName3 }))
-      .then(() => expect(() => storage.Buckets.delete(bucketId3), 'to be fulfilled'))
-      .then(() => expect(() => storage.request('get', `buckets/${bucketId3}`), 'to be rejected')));
+  it('delete', () => expect(() => storage.Buckets.delete(bucketId3), 'to be rejected')
+    .then(() => storage.request('post', 'buckets', { stage: 'in', name: bucketName3 }))
+    .then(() => expect(() => storage.Buckets.delete(bucketId3), 'to be fulfilled'))
+    .then(() => expect(() => storage.request('get', `buckets/${bucketId3}`), 'to be rejected')));
 });
--- test/Storage.js
+++ test/Storage.js
@@ -4,30 +4,27 @@ const expect = require('unexpected');
 
 describe('Storage', () => {
   const storage = new Storage(process.env.KBC_URL, process.env.KBC_TOKEN);
-  it('request', () =>
-    storage.request('get', 'tokens/verify')
-      .then((res) => {
-        expect(res, 'to have key', 'id');
-        expect(res, 'to have key', 'token');
-        expect(res, 'to have key', 'owner');
-        expect(res.token, 'to be', process.env.KBC_TOKEN);
-      }));
+  it('request', () => storage.request('get', 'tokens/verify')
+    .then((res) => {
+      expect(res, 'to have key', 'id');
+      expect(res, 'to have key', 'token');
+      expect(res, 'to have key', 'owner');
+      expect(res.token, 'to be', process.env.KBC_TOKEN);
+    }));
 
-  it('verifyToken', () =>
-    storage.verifyToken()
-      .then((res) => {
-        expect(res, 'to have key', 'id');
-        expect(res, 'to have key', 'token');
-        expect(res, 'to have key', 'owner');
-        expect(res.token, 'to be', process.env.KBC_TOKEN);
-      }));
+  it('verifyToken', () => storage.verifyToken()
+    .then((res) => {
+      expect(res, 'to have key', 'id');
+      expect(res, 'to have key', 'token');
+      expect(res, 'to have key', 'owner');
+      expect(res.token, 'to be', process.env.KBC_TOKEN);
+    }));
 
-  it('verifyTokenAdmin', () =>
-    storage.verifyTokenAdmin()
-      .then((res) => {
-        expect(res, 'to have key', 'id');
-        expect(res, 'to have key', 'token');
-        expect(res, 'to have key', 'owner');
-        expect(res.token, 'to be', process.env.KBC_TOKEN);
-      }));
+  it('verifyTokenAdmin', () => storage.verifyTokenAdmin()
+    .then((res) => {
+      expect(res, 'to have key', 'id');
+      expect(res, 'to have key', 'token');
+      expect(res, 'to have key', 'owner');
+      expect(res.token, 'to be', process.env.KBC_TOKEN);
+    }));
 });
