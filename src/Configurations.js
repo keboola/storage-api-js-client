56f4af54dae101640fbe5f0e1e2411c2866eadfb
Jakub Matejka <jakub@keboola.com>
2018-10-05 13:00:24 +0200

    feat: add Configurations support

---
+++ src/Configurations.js
@@ -0,0 +1,51 @@
+// @flow
+import _ from 'lodash';
+import Storage from './Storage';
+
+class Configurations {
+  storage: Storage;
+
+  constructor(storage: Object) {
+    this.storage = storage;
+  }
+
+  async create(
+    componentName: string,
+    name: string,
+    options: ?{
+      configurationId: ?string,
+      description: ?string,
+      configuration: ?Object,
+      state: ?Object,
+      changeDescription: ?string,
+    }
+  ): Promise<string> {
+    let params = { name };
+    if (options) {
+      params = _.merge(params, options);
+      if (_.has(params, 'configuration')) {
+        params.configuration = JSON.stringify(params.configuration);
+      }
+      if (_.has(params, 'state')) {
+        params.state = JSON.stringify(params.state);
+      }
+    }
+
+    const res = await this.storage.request(
+      'post',
+      `components/${componentName}/configs`,
+      params
+    );
+    return res.id;
+  }
+
+  get(componentName: string, id: string): Promise<Object> {
+    return this.storage.request('get', `components/${componentName}/configs/${id}`);
+  }
+
+  delete(componentName: string, id: string): Promise<Object> {
+    return this.storage.request('delete', `components/${componentName}/configs/${id}`);
+  }
+}
+
+module.exports = Configurations;
